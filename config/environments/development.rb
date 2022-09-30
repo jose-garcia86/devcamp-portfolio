f11b392a2cb0d31caf1893381df2bf9df4dbd269
Jose Garcia <jose.garcia.dev86@gmail.com>
2022-09-30 22:53:32 +0200

    Implemented basic authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Default url options for development
+  # In production, :host should be set to the actual host of your application
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
